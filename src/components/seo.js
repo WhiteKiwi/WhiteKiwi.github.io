b4a3c09cc61f4c5b147643468a671a1c78839440
whitekiwi <jh145478@gmail.com>
2020-09-04 00:03:07 +0900

    add bootstrap

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function SEO({ title }) {
     return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "../styles/index.scss"
+import Alert from "react-bootstrap/Alert"
 
 export default function Home() {
 	return (
@@ -9,6 +10,22 @@ export default function Home() {
 			<SEO title="Kiwi's Portfolio" />
 
 			<h1>Hello world!</h1>
+			{
+				[
+					"primary",
+					"secondary",
+					"success",
+					"danger",
+					"warning",
+					"info",
+					"light",
+					"dark",
+				].map((variant, idx) => (
+					<Alert key={idx} variant={variant}>
+						This is a {variant} alert—check it out!
+					</Alert>
+				))
+			}
 		</Layout>
 	)
 }
