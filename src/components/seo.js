36a2e5d057232692f5bac76a7cbd3a2ebbb3eb13
kiwi-jang <kiwi.jang@toss.im>
2022-08-05 17:15:42 +0900

    ga 테스트

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,12 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import React from "react"
 import { Helmet } from "react-helmet"
-import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/style.scss"
 
 export default function SEO({ title }) {
-    return (
-        <Helmet>
-            <title>{title}</title>
-        </Helmet>
-    )
+  return (
+    <Helmet>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-7WPVHL3XBS"
+      ></script>
+      <script>{`
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+          dataLayer.push(arguments)
+      }
+      gtag('js', new Date()); gtag('config', 'G-7WPVHL3XBS');
+      `}</script>
+      <title>{title}</title>
+    </Helmet>
+  )
 }
